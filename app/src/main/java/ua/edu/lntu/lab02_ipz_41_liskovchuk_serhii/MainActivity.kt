c0e2f1a2572b08e47f4d13b99c07cd639aaaf7d5
mickey394 <tliskovchuk@gmail.com>
2024-03-12 13:43:21 +0200

    commit

---
+++ app/src/main/java/ua/edu/lntu/lab02_ipz_41_liskovchuk_serhii/MainActivity.kt
@@ -0,0 +1,46 @@
+package ua.edu.lntu.lab02_ipz_41_liskovchuk_serhii
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ua.edu.lntu.lab02_ipz_41_liskovchuk_serhii.ui.theme.Lab02_IPZ_41_Liskovchuk_serhiiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab02_IPZ_41_Liskovchuk_serhiiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab02_IPZ_41_Liskovchuk_serhiiTheme {
+        Greeting("Android")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lab02_IPZ_41_Liskovchuk_serhii"
+include(":app")
+ 
